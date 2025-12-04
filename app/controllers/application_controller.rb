54582e19fdf9de1f1e62904c94ec74544375d4e7
omsking <olive.msk8@gmail.com>
2025-12-04 00:42:59 +0000

    fixed sign-up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+
+  before_action :configure_permitted_parameters, { :if => :devise_controller? }
+
+  def configure_permitted_parameters
+    added_attrs = [:phone_number, :birth_date, :location, :birth_time, :text_time]
+
+    devise_parameter_sanitizer.permit(:sign_up, :keys => added_attrs)
+    devise_parameter_sanitizer.permit(:account_update, :keys => added_attrs)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -32,9 +32,9 @@ class User < ApplicationRecord
   has_many  :daily_weathers, class_name: "DailyWeather", foreign_key: "user_id", dependent: :destroy
   has_many  :daily_horoscopes, class_name: "DailyHoroscope", foreign_key: "user_id", dependent: :destroy
 
-  validates :text_time, presence: true
+ # validates :text_time, presence: true
   validates :phone_number, format: { with: /\A[0-9]{10}\z/ }
   validates :phone_number, uniqueness: { case_sensitive: false, message: "This phone number has already been registered. Please sign in." }
-  validates :location, presence: true
+ # validates :location, presence: true
   validates :birth_date, presence: true
 end
